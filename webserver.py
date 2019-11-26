1e2505b7662ccb70ca4e25c3b3194243ec9b9602
Your Name <matteoi@al.insper.edu.br.com>
2019-11-26 00:57:57 -0300

    crud fix

--- webserver.py
+++ webserver.py
@@ -7,13 +7,12 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from pymongo import MongoClient
+from bson import ObjectId
 
 app = Flask(__name__)
 client = MongoClient('mongodb://' + os.environ["REDIRECTIP"] + "/database", 27017)
-# dicionario = globaldict()
 db = client.database
 tarefas = db.tarefas
-# redirecionador = redirector(os.environ["REDIRECTIP"])
 
 @app.route('/Tarefa', methods=['GET', 'POST'])
 def Tarefa():
@@ -25,38 +24,23 @@ def Tarefa():
             listinha.append(i)
         response = {"resposta" : listinha}
         return response
-        # return redirecionador.redirect_request("listar")
-        # return dicionario.dict
+
     if request.method == 'POST':
-        return tarefas.insert(request.form)
-        # return redirecionador.redirect_request("adicionar", request.form)
-        # dicionario.add_coisa(request.form)
-        # resp = jsonify(success=True)
-        # return resp
+        return str(tarefas.insert(request.form.to_dict()))
 
 @app.route('/Tarefa/<id>', methods=['GET', 'PUT', 'DELETE'])
 def Tarefa_id(id):
     if request.method == 'GET':
-        return tarefas.find({"_id":id})[0]
+        response = tarefas.find({"_id":ObjectId(id)})
+        response = response[0]
+        response["_id"] = str(response["_id"])
+        return response
 
-        # return redirecionador.redirect_request("buscar", params=[id])
-        # resp = dicionario.get_coisa(id)
-        # if (resp != False):
-        #     return resp
-        # resp = jsonify(success = False)
-        # return resp
     if request.method == 'PUT':
-        return tarefas.update({"_id":id}, {"$set":request.form})
-        # return redirecionador.redirect_request("atualizar", params=[id, request.form])
-        # resp = dicionario.update_coisa(id, request.form)
-        # resp = jsonify(success=(resp != False))
-        # return resp
+        return str(tarefas.update({"_id":ObjectId(id)}, {"$set":request.form.to_dict()}))
+
     if request.method == 'DELETE':
-        return tarefas.delete_one({"_id":id})
-        # return redirecionador.redirect_request("apagar", params=[id])
-        # resp = dicionario.deleta_coisa(id)
-        # resp = jsonify(success=(resp != False))
-        # return resp
+        return str(tarefas.delete_one({"_id":ObjectId(id)}))
 
 @app.route('/')
 def healthcheck():
